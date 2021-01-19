6c084fe98d06b0c67d76f3128401691f6e1b0cfc
chcho2 <chcho87@gmail.com>
2021-01-18 22:41:16 -0500

    updated css  in jobdetail.js

--- components/JobPosting/JobDetail.js
+++ components/JobPosting/JobDetail.js
@@ -6,15 +6,19 @@ const JobDetail = (props) => {
   return (
     <div className="w-full mt-5 h-full bg-white">
       <div className=" w-10/12 ">
+        <div className="">
           <div
             className="table-row grid-cols-3 divansform scale-100
-                    text-xs py-1 cursor-default bg-white bg-opacity-25">
-            <div className="table-row grid md:flex relative items-center whitespace-flex-wrap font-semibold text-black">
+                    text-xs py-1  cursor-default 
+
+            bg-white bg-opacity-25"
+          >
+            <div className="table-row grid col-span-2 md:flex relative items-center whitespace-flex-wrap font-semibold text-black">
               <div className="mr-32">Department:</div>
               <div className="leading-5 table-cell py-3 text-gray-500 font-medium">
                 {dept}
               </div>
-           
+            </div>
 
             <div className="relative grid md:flex table-row items-center whitespace-flex-wrap font-semibold text-black">
               <div className="mr-32 whitespace-nowrap">Hours / shifts:</div>
